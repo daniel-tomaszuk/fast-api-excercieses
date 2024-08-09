0165f65f6dcc191e465f165183cd68dd7ee69f16
Daniel Tomaszuk <dtomaszuk01@gmail.com>
2024-08-09 13:49:19 +0200

    Add user creation endpoint with password hashing.

--- todo_project/routers/auth.py
+++ todo_project/routers/auth.py
@@ -1,9 +1,26 @@
+import bcrypt
 from fastapi import APIRouter
+from fastapi import status
 
+from todo_project.dependencies import db_dependency
+from todo_project.models import Users
+from todo_project.serializers.serializers import CreateUserRequest
 
 router = APIRouter()
 
 
-@router.get("/auth/")
-async def get_user():
-    return dict(user="authenticated")
+@router.post("/auth/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    hashed: bytes = bcrypt.hashpw(str(create_user_request.password).encode("UTF-8"), bcrypt.gensalt())
+    hashed: str = hashed.decode("UTF-8")
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=hashed,
+        is_active=True,
+    )
+    db.add(create_user_model)
+    db.commit()
--- todo_project/serializers/serializers.py
+++ todo_project/serializers/serializers.py
@@ -1,5 +1,12 @@
+from enum import StrEnum
+
 from pydantic import BaseModel
 from pydantic import Field
+
+
+class UserRoleEnumerate(StrEnum):
+    admin = "admin"
+    user = "user"
 
 
 class TodoRequest(BaseModel):
@@ -7,3 +14,12 @@ class TodoRequest(BaseModel):
     description: str | None = Field(max_length=100)
     priority: int = Field(ge=0, le=5)
     complete: bool = False
+
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: UserRoleEnumerate = UserRoleEnumerate.user
